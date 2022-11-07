8aa28afbe2f555a2fd9e9a4f932a4fb431b4f04b
Gautzilla <gauthier.berthomieu@gmail.com>
2022-11-07 14:53:02 +0100

    Ajoute le programme fileRenaming
    renomme les fichiers .wav pour qu'ils soient triés dans le bon ordre (nombre de micros) dans le polybuffer maxMSP

---
+++ testEcoute/fileRenaming/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace fileName
+{
+    class Program
+    {
+        // Pads the nMic value for maxMSP's polybuffer to sort the names numerically
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\User\Documents\Gaut\PostDoc\Manips\Aliasing Spatial\Preview");
+            FileInfo[] files = dir.GetFiles();
+            
+            foreach (FileInfo file in files)
+            {
+                Match name = Regex.Match(file.Name, @"(?<source>\w+)_(?<room>\w+)_0deg_(?<pattern>\w+)_(?<nMic>\d+)_(?<radius>\d+cm)_o7_binaural.wav$");
+                if (name.Value == "") continue;
+
+                string newName = $"{name.Groups["source"].Value}_{name.Groups["room"].Value}_0deg_{name.Groups["pattern"].Value}_{name.Groups["nMic"].Value.PadLeft(3, '0')}_{name.Groups["radius"].Value}_o7_binaural.wav";
+            
+                File.Move(file.FullName, file.FullName.Replace(name.Value, newName));
+            }
+        }
+    }
+}
